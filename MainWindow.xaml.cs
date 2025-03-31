d8684f99af5600ef09b2c7349e81d5497ca7e129
heyongxian <heyongxian@kuaishou.com>
2025-03-31 15:13:46 +0800

    commit

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Drawing.Imaging;
 using System.IO;
 using System.IO.Compression;
 using System.Reflection;
@@ -10,6 +12,7 @@ using System.Windows.Controls;
 using System.Windows.Input;
 using System.Windows.Markup;
 using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
 using Microsoft.Extensions.Configuration;
 namespace stock_tool;
 
@@ -41,8 +44,6 @@ public partial class MainWindow : Window
 
     private DateTime lastClickTime = DateTime.MinValue;
     private const int clickInterval = 2000; // 点击间隔时间，单位为毫秒
-
-    private bool debug = false;
 
     public MainWindow()
     {
@@ -222,8 +223,124 @@ public partial class MainWindow : Window
 
     }
 
+    public static Bitmap 白框(System.Drawing.Image img, int whiteSize)
+    {
+        System.Drawing.Size size = img.Size;
+        int num = Math.Max(size.Width, size.Height);
+        int num2 = Math.Max(0, whiteSize - num);
+        size.Width += num2;
+        size.Height += num2;
+        Bitmap bitmap = new Bitmap(size.Width, size.Height);
+        using Graphics graphics = Graphics.FromImage(bitmap);
+        graphics.InterpolationMode = InterpolationMode.HighQualityBicubic;
+        graphics.SmoothingMode = SmoothingMode.HighQuality;
+        graphics.Clear(Color.White);
+        graphics.DrawImage(img, new Rectangle(50, 50, bitmap.Width - 100, bitmap.Height - 100), new Rectangle(0, 0, img.Width, img.Height), GraphicsUnit.Pixel);
+        return bitmap;
+    }
+    
+        
+
+        private void ProcessFile(string filePath)
+        {
+            try
+            {
+                using (System.Drawing.Image image = System.Drawing.Image.FromFile(filePath))
+                {
+                    using MemoryStream memoryStream = new MemoryStream();
+                    Save(memoryStream, image);
+                    memoryStream.Seek(0L, SeekOrigin.Begin);
+                    byte[] buffer = new byte[4096];
+                    using (FileStream fileStream = File.Open(filePath, FileMode.Create, FileAccess.Write, FileShare.Read))
+                    {
+                        int count;
+                        while ((count = memoryStream.Read(buffer, 0, 4096)) > 0)
+                        {
+                            fileStream.Write(buffer, 0, count);
+                        }
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"处理图片文件 {filePath} 时出错: {ex.Message}");
+            }
+        }
+
+
+    public static void Save(Stream sr, System.Drawing.Image img)
+    {
+        Bitmap bitmap = null;
+        EncoderParameter encoderParameter = null;
+        EncoderParameters encoderParameters = null;
+        try
+        {
+            bitmap = new Bitmap(img.Width, img.Height);
+            using (Graphics graphics = Graphics.FromImage(bitmap))
+            {
+                graphics.DrawImage(img, 0, 0);
+            }
+
+            encoderParameters = new EncoderParameters(1);
+            encoderParameter = new EncoderParameter(System.Drawing.Imaging.Encoder.Quality, 95L);
+            encoderParameters.Param[0] = encoderParameter;
+            bitmap.Save(sr, ImageCodecInfo.GetImageEncoders()[1], encoderParameters);
+        }
+        finally
+        {
+            encoderParameter?.Dispose();
+            encoderParameters?.Dispose();
+            bitmap?.Dispose();
+        }
+    }
+
     private void WhiteClick(object sender, RoutedEventArgs e)
     {
+        // 获取用户输入的源文件夹路径
+        string sourceFolder = _configuration["ImagePath"];
+        string timestamp = DateTime.Now.ToString("yyyy_MM_dd_HH_mm_ss");
+
+
+        try
+        {
+            if (!Directory.Exists(sourceFolder))
+            {
+                Logger.Error($"白框失败，源文件夹 {sourceFolder} 不存在");
+                return;
+            }
+            // 获取源文件夹的上一级目录
+            DirectoryInfo parentDir = Directory.GetParent(sourceFolder);
+            if (parentDir != null)
+            {
+                string parentPath = parentDir.FullName;
+                string zipFileName = Path.Combine(parentPath, $"image_{timestamp}.zip");
+
+                Logger.Info($"开始压缩..{sourceFolder}");
+                ZipFile.CreateFromDirectory(sourceFolder, zipFileName);
+                Logger.Info($"压缩成功: {zipFileName}");
+            }
+        }
+        catch (Exception ex)
+        {
+            Logger.Error($"压缩失败: {ex.Message}");
+        }
+
+        try
+        {
+            var allFiles = Directory.EnumerateFiles(sourceFolder, "*", SearchOption.AllDirectories);
+            foreach (var file in allFiles)
+            {
+                ProcessFile(file);
+            }
+        }
+        catch (UnauthorizedAccessException)
+        {
+            MessageBox.Show("没有权限访问某些文件夹或文件。");
+        }
+        catch (DirectoryNotFoundException)
+        {
+            MessageBox.Show("指定的文件夹未找到。");
+        }
 
     }
 
@@ -268,7 +385,8 @@ public partial class MainWindow : Window
        
     }
 
-    private void ProcessSingle(AutomationElement mainWindow, AutomationElement targetWindow, int time = 0) {
+    private void ProcessSingle(AutomationElement mainWindow, AutomationElement targetWindow,
+        int time = 0, bool submit = true, string right = "StockRight") {
         Logger.Info("开始执行....");
         
         // 假设要点击窗口内的坐标 (100, 200)，可根据实际情况修改
@@ -321,13 +439,13 @@ public partial class MainWindow : Window
                         throw new Exception("ID错误");
                     }
           
-                    int stock = (int)targetWindow.Current.BoundingRectangle.Right - ConvertFromConfig("StockRight", true);
+                    int stock = (int)targetWindow.Current.BoundingRectangle.Right - ConvertFromConfig(right, true);
 
                     MouseSimulator.Click(stock, y);
                     Logger.Info($"{id.Current.Name},第{i}个,点击库存图标 x:{x} y:{y}");
                     last_id = id.Current.Name;
                    
-                    if (debug)
+                    if (submit)
                     {
                         Thread.Sleep(200);
                         StockInput.PressY();
@@ -437,12 +555,84 @@ public partial class MainWindow : Window
     private void WhiteTest(object sender, RoutedEventArgs e)
     {
 
+        DateTime now = DateTime.Now;
+        if ((now - lastClickTime).TotalMilliseconds < clickInterval)
+        {
+            Logger.Info($"请{clickInterval / 1000}秒后重试");
+            return;
+        }
+
+        lastClickTime = now;
+
+        // 获取配置项
+        string targetWindowTitle = _configuration["TargetWindowTitle"];
+        string targetElementTitle = _configuration["TargetElementTitle"];
+
+        // 查找目标窗口
+        AutomationElement mainWindow = AutomationElement.RootElement.FindFirst(
+                    TreeScope.Children,
+                    new PropertyCondition(AutomationElement.NameProperty, targetWindowTitle));
+
+        if (mainWindow == null)
+        {
+            Logger.Info($"找不到【{targetWindowTitle}】，请运行程序");
+            return;
+        }
+
+        AutomationSearchHelper.TryActivateWindow(mainWindow);
+        max(mainWindow);
+        // 查找右边pannel
+        AutomationElement targetWindow = mainWindow.FindFirst(TreeScope.Descendants,
+            new PropertyCondition(AutomationElement.NameProperty, targetElementTitle));
+        if (targetWindow == null)
+        {
+            Logger.Info($"找不到 【{targetElementTitle}】,请打开某商品并点击【库存】");
+            return;
+        }
+        ProcessSingle(mainWindow, targetWindow, 0, true, "WhiteRight");
     }
 
     private void PageTest(object sender, RoutedEventArgs e)
     {
+        DateTime now = DateTime.Now;
+        if ((now - lastClickTime).TotalMilliseconds < clickInterval)
+        {
+            Logger.Info($"请{clickInterval / 1000}秒后重试");
+            return;
+        }
 
+        Logger.Info("开始处理所有....");
+        // 获取配置项
+        string targetWindowTitle = _configuration["TargetWindowTitle"];
+        string targetElementTitle = _configuration["TargetElementTitle"];
+
+        // 查找目标窗口
+        AutomationElement mainWindow = AutomationElement.RootElement.FindFirst(
+                    TreeScope.Children,
+                    new PropertyCondition(AutomationElement.NameProperty, targetWindowTitle));
+
+        if (mainWindow == null)
+        {
+            Logger.Info($"找不到【{targetWindowTitle}】，请运行程序");
+            return;
+        }
+        AutomationSearchHelper.TryActivateWindow(mainWindow);
+        max(mainWindow);
+        // 查找右边pannel
+        AutomationElement targetWindow = mainWindow.FindFirst(TreeScope.Descendants,
+            new PropertyCondition(AutomationElement.NameProperty, targetElementTitle));
+
+
+        if (targetWindow == null)
+        {
+            Logger.Info($"找不到 【{targetElementTitle}】,请打开某商品");
+            return;
+        }
+        int maxCount = GetMaxCount(mainWindow);
+        ProcessSingle(mainWindow, targetWindow, maxCount, false);
     }
+
+
     private void StockTest(object sender, RoutedEventArgs e)
     {
 
